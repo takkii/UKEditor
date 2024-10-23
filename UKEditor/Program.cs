3fa8ad4709c5ddd40263119efb5d00bd140f1e0d
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-10-23 13:00:49 +0900

    Update.

--- UKEditor/Program.cs
+++ UKEditor/Program.cs
@@ -3,6 +3,20 @@ using System.Windows.Forms;
 
 namespace UKEditor
 {
+    class Weathertype
+    {
+        public string baseUrl;
+        public string city;
+        public string url;
+        public string json;
+        public string reiwa_kanji;
+        public string OneYear;
+        public string OneMonth;
+        public string Onedays;
+        public string OneHour;
+        public string OneMinutes;
+        public string Oneseconds;
+    }
     static class Program
     {
         /// <summary>
